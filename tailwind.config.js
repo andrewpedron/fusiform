dcee230491f21cf427d65570b97159765b6951ce
Andrew Pedron <andrewpedron@Andrews-MacBook-Pro-3.local>
2025-02-01 21:09:04 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Make sure this points to your HTML and JS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
